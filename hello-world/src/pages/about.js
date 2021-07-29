a1d027fb472de971d40cf1f6c839955228fc16b2
Bradley Morgan <brad@bradleys-mbp.lan>
2021-07-28 19:19:18 -0700

    init commit

---
+++ hello-world/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/headers"
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+        <Header headerText="Welcome back Succkksaz" />
+    <Header headerText="Another Header" />
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
---
+++ hello-world/src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "gatsby";
+
+export default function Home() {
+  return <div>
+    <Link to="/contact">Contact</Link>
+    <h1>Luna is my homie</h1></div>
+}
