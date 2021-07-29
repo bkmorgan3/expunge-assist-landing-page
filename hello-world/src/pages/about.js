125d075d1d9ad0f213bb16c32e32d051560fdb53
Bradley Morgan <brad@bradleys-mbp.lan>
2021-07-28 23:04:22 -0700

    Setsup pages, add Links

--- hello-world/src/pages/about.js
+++ hello-world/src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Header from "../components/headers"
+import Header from "../components/headers";
+import { Link } from "gatsby";
 
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
-        <Header headerText="Welcome back Succkksaz" />
+      <Link to="/">Home</Link>
+        <Header headerText="This is great." />
     <Header headerText="Another Header" />
       <p>Such wow. Very React.</p>
     </div>
--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -4,5 +4,7 @@ import { Link } from "gatsby";
 export default function Home() {
   return <div>
     <Link to="/contact">Contact</Link>
-    <h1>Luna is my homie</h1></div>
+    <h1>Luna is my homie</h1>
+       <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
 }
