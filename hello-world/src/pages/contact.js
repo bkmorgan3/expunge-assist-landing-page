113a39178a2436b04ec891b47acc98986ce46f48
Brad Morgan <bkmorgan3@gmail.com>
2021-08-02 11:40:48 -0700

    styles contact form

--- hello-world/src/pages/contact.js
+++ hello-world/src/pages/contact.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Header from "../components/headers";
 import Nav from "../components/Nav";
+import ContactImg from "../assets/images/contact.png";
 
 import styled from "styled-components";
 
@@ -13,13 +14,48 @@ const ContactContainer = styled.div`
 `;
 
 const HeaderContainer = styled.div`
-  border: 1px solid tan;
   display:flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
   font-family: 'Roboto';
   font-size: 1.5em;
+`;
+
+const ContentContainer = styled.div`
+  width: 100%;
+  display: flex;
+  height: 50%;
+`;
+
+const ImageContainer = styled.div`
+  width: 50%;
+`;
+const FormContainer = styled.div`
+  width: 50%;
+  border: 1px solid blue;
+  margin-right: 10px;
+`;
+
+const Image = styled.img`
+  width: 70%;
+`;
+
+const RadioButtonContainer = styled.div`
+  width: 100%;
+  border: 1px solid red;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const InputContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid purple;
+  height: 200px;
 `;
 
 export default function Contact() {
@@ -30,8 +66,13 @@ export default function Contact() {
         <h2>Interested in what we're doing ?</h2>
         <h2>Send us an email at <b>info@expungeassist.org</b></h2>
       </HeaderContainer>
-      <form action="https://getform.io/f/c2344ce2-c2ca-4fce-ba76-1bdefd869586" method="post">
-       <div>
+      <ContentContainer>
+        <ImageContainer>
+          <Image src={ContactImg} alt=" man" />
+        </ImageContainer>
+        <FormContainer>
+           <form action="https://getform.io/f/c2344ce2-c2ca-4fce-ba76-1bdefd869586" method="post">
+       <RadioButtonContainer>
          <label htmlFor="partnership">
            Interested in Partnership
            <input type="radio" />
@@ -40,22 +81,27 @@ export default function Contact() {
            Interested in Volunteering?
            <input type="radio" />
          </label>
-       </div>
+       </RadioButtonContainer>
+       <InputContainer>
+       
         <label htmlFor="name">
           Name
           <input type="text" name="name" />
         </label>
+
         <label htmlFor="email">
           Email
           <input type="email" name="email" />
+          </label>
           <label htmlFor="message">
             Message
-            <textarea name="message">
-
-            </textarea>
-          </label>
+            <textarea placeholder="Fuck off" name="message" />
         </label>
+        </InputContainer>
       </form>
+        </FormContainer>
+      </ContentContainer>
+     
     </ContactContainer>
   )
 }
