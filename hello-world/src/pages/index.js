38a6cbdc53dc8db79867868903ec785ac1a1376a
Brad Morgan <bkmorgan3@gmail.com>
2021-08-09 23:32:29 -0700

    Adds first form in Partners section

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -67,6 +67,96 @@ const PartnersContainer = styled.div`
   background-color: #fff;
 `
 
+const PartnersFormSection = styled.div`
+  width: 100%;
+  height: 500px;
+  display: flex;
+`
+
+const PartnerFormContainer = styled.div`
+  width: 60%;
+`
+
+const ColorsContainer = styled.div`
+  width: 40%;
+`
+
+const PartnerForm = styled.div`
+  height: 375px;
+  width: 700px;
+  border-radius: 8px;
+  margin-left: 150px;
+  margin-top: 30px;
+  border: 8px solid rgba(245, 179, 4, 0.66);
+  background-color: #fffaf2;
+  font-size: 1.6em;
+`
+
+const HeadingContainer = styled.div`
+  width: 50%;
+  margin-left: 100px;
+`
+
+const PartnerFormHeading = styled.h2`
+  font-family: "mulish";
+`
+
+const WhyPartnersContainer = styled.div`
+  width: 100%;
+  border: 1px solid red;
+  height: 300px;
+`
+
+const PartnerMissionContainer = styled.div`
+  width: 100%;
+  border: 1px solid blue;
+  height: 300px;
+`
+
+const PurpledLetters = styled.span`
+  color: #9903ff;
+`
+
+const PartnerFormText = styled.p`
+  font-family: "mulish";
+  margin-left: 100px;
+`
+
+const PartnerTextAction = styled.span`
+  font-size: 0.9em;
+  margin-left: 50px;
+`
+
+const Form = styled.form`
+  display: flex;
+  justify-content: space-evenly;
+`
+
+const Input = styled.input`
+  border: 2px solid #000;
+  background: #fff;
+  height: 42px;
+  width: 447px;
+`
+
+const Label = styled.label`
+  font-size: 16px;
+  line-height: 16px;
+  color: #373f41;
+  margin-bottom: 5px;
+  font-family: "Mulish";
+`
+
+const SubmitButton = styled.button`
+  border: none;
+  border-radius: 2px;
+  background-color: #9903ff;
+  width: 152px;
+  height: 47px;
+  color: #fff;
+  font-size: 0.8em;
+`
+
 export default function Home() {
   useEffect(() => {
     document.title = `Expunge Assist Homepage`
@@ -96,7 +186,38 @@ export default function Home() {
           <GreetingImage src={laptop} alt="" />
         </GreetingImageContainer>
       </GreetingContainer>
-      <PartnersContainer></PartnersContainer>
+      <PartnersContainer>
+        <PartnersFormSection>
+          <PartnerFormContainer>
+            <PartnerForm>
+              <HeadingContainer>
+                <PartnerFormHeading>
+                  We're looking for <PurpledLetters>Partners</PurpledLetters> to
+                  help!
+                </PartnerFormHeading>
+              </HeadingContainer>
+              <PartnerFormText>Lorem Ipsum</PartnerFormText>
+              <PartnerFormText>Ipsum Lorem</PartnerFormText>
+              <PartnerTextAction>
+                Sign up for more about becoming a partner and for project
+                updates.
+              </PartnerTextAction>
+              <Form action="#" method="POST">
+                <Label htmlFor="signup"></Label>
+                <Input
+                  placeholder="name@domain.com"
+                  name="signup"
+                  type="email"
+                ></Input>
+                <SubmitButton>Register</SubmitButton>
+              </Form>
+            </PartnerForm>
+          </PartnerFormContainer>
+          <ColorsContainer></ColorsContainer>
+        </PartnersFormSection>
+        <WhyPartnersContainer></WhyPartnersContainer>
+        <PartnerMissionContainer></PartnerMissionContainer>
+      </PartnersContainer>
       <Footer />
     </AppContainer>
   )
